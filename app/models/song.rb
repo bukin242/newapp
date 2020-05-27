e1933caeffd81591be9e9a236205f406e0965d15
Aleksey Bukin <bukin242@yandex.ru>
2020-05-28 02:12:52 +0500

    add top songs

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,24 @@
 class Song < ActiveRecord::Base
   default_scope { order('title asc') }
   belongs_to :artist
+
+  def self.top(days, count)
+    start_date = (DateTime.current - days.days).strftime('%Y-%m-%d %H:%M:%S')
+    end_date = DateTime.current.strftime('%Y-%m-%d %H:%M:%S')
+
+    sql = <<-SQL
+      with suitable_songs as (
+        select downloads.song_id as song_id, count(downloads.song_id) as count from songs
+          join downloads on downloads.song_id = songs.id
+         where downloads.created_at >= '#{start_date}' and downloads.created_at < '#{end_date}'
+         group by downloads.song_id
+      )
+      select * from songs
+        join suitable_songs on songs.id = suitable_songs.song_id
+       order by suitable_songs.count desc
+       limit #{count}
+    SQL
+
+    Song.find_by_sql(sql)
+  end
 end
